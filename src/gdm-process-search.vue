03be64f35bbdff95b46707424b4a200edb94762f
epointal <elisabeth.pointal@free.fr>
2020-04-02 18:30:52 +0200

    continue

--- src/gdm-process-search.vue
+++ src/gdm-process-search.vue
@@ -21,69 +21,71 @@
 <template>
   <span class="gdm-process-search">
   <div class="wrapper">
-  <div v-if="features.length === 0" class="message">
-  {{$t('no_process')}}
-  </div>
-  <div v-else>
-	   <gdm-form-process :user="parameters.user" :service="parameters.service" @remove="removeSelected"></gdm-form-process> 
-	   <gdm-paging :start-index="pagination.startIndex" :max-records="pagination.maxRecords"  
-	   :count="pagination.count" :total-results="pagination.totalResults"
-	   :lang="lang" :color="color" @change="pageChange"></gdm-paging>
-	   <table>
-	     <thead :style="{background:$shadeColor(color, 0.5)}">
-	     <th>{{$t('identifiers')}}</th>
-	     <th>Status</th>
-	     <th>{{$t('process_dates')}}</th>
-	     <th>{{$t('temporal_extent')}}</th>
-	     <th>{{$t('parameters')}}</th>
-	     
-	     </thead>
-	     <tbody>
-	     <tr v-for="feature in features">
-	     <td>
-		     <div><b>{{feature.properties.id}}</b></div>
-		     <div class="gdm-token" v-if="feature.properties.token">{{feature.properties.token}}</div>
-         
-		     <div class="toSelect" :class="{selected: parameters.service}" 
-	           @click="selectService({id:feature.properties.serviceId, name:feature.properties.serviceName})">
-	           {{feature.properties.serviceName}}
-	       </div>
-		     <div v-if="userId">{{feature.properties.email}}</div>
-		     <div v-else>
-		         <div class="toSelect" :class="{selected: parameters.user}" 
-		         @click="selectUser({id:feature.properties.userId, email:feature.properties.email})">
-		            {{feature.properties.email}}
-		          </div>
+	  <gdm-form-process :lang="lang" :color="color" :user="parameters.user" :service="parameters.service"
+	   @remove="removeSelected" @dateChange="dateChange" @statusChange="statusChange"></gdm-form-process> 
+	    
+	  <div v-if="features.length === 0" class="message">
+	      {{$t('no_process')}}
+	   </div>
+	   <div v-else>
+		   <gdm-paging :start-index="pagination.startIndex" :max-records="pagination.maxRecords"  
+		   :count="pagination.count" :total-results="pagination.totalResults"
+		   :lang="lang" :color="color" @change="pageChange"></gdm-paging>
+		   <table>
+		     <thead :style="{background:$shadeColor(color, 0.5)}">
+		     <th>{{$t('identifiers')}}</th>
+		     <th>Status</th>
+		     <th>{{$t('process_dates')}}</th>
+		     <th>{{$t('temporal_extent')}}</th>
+		     <th>{{$t('parameters')}}</th>
+		     
+		     </thead>
+		     <tbody>
+		     <tr v-for="feature in features">
+		     <td>
+			     <div><b>{{feature.properties.id}}</b></div>
+			     <div class="gdm-token" v-if="feature.properties.token">{{feature.properties.token}}</div>
+	         
+			     <div class="toSelect" :class="{selected: parameters.service}" 
+		           @click="selectService({id:feature.properties.serviceId, name:feature.properties.serviceName})">
+		           {{feature.properties.serviceName}}
+		       </div>
+			     <div v-if="userId">{{feature.properties.email}}</div>
+			     <div v-else>
+			         <div class="toSelect" :class="{selected: parameters.user}" 
+			         @click="selectUser({id:feature.properties.userId, email:feature.properties.email})">
+			            {{feature.properties.email}}
+			          </div>
+			     </div>
+		      
+			     <div v-if="feature.properties.processusName">({{feature.properties.processusName}})</div>
+		     </td>
+		     <td style="text-align:center;cursor:pointer;" :title="feature.properties.log">
+		        <span :class="statusToClass(feature.properties.status)"></span>
+		        <div style="font-style:italic;font-size:0.9rem;color:grey;">{{feature.properties.status}}</div>
+		        <a v-if="back"  :href="launchUrl + 'process/launch/' + feature.properties.id" class="button">Test Curl</a>
+		     <td style="text-align:left;">
+		     <b>Start: </b>{{printDate(feature.properties.processStart,true)}}<br/>
+		     <b>End: </b>{{printDate(feature.properties.processEnd, true)}}
+		     </td>
+		     
+		     <td style="text-align:center;">
+		     {{printDate(feature.properties.temporalExtent[0])}}
+		     <b>&rarr;</b>
+		     {{printDate(feature.properties.temporalExtent[1])}}
+		     </td>
+		     <td>
+		     <div v-for="type in ['provider', 'position', 'parameters']" class="infos" >
+		         <div v-if="feature.properties[type]"  >
+		            <div v-for="(value, prop) in feature.properties[type]" >
+		              <div><b>{{prop}}:</b> {{value}}</div>
+		            </div>
+		         </div>
 		     </div>
-	      
-		     <div v-if="feature.properties.processusName">({{feature.properties.processusName}})</div>
-	     </td>
-	     <td style="text-align:center;cursor:pointer;" :title="feature.properties.log">
-	        <span :class="statusToClass(feature.properties.status)"></span>
-	        <div style="font-style:italic;font-size:0.9rem;color:grey;">{{feature.properties.status}}</div>
-	        <a v-if="back"  :href="launchUrl + 'process/launch/' + feature.properties.id" class="button">Test Curl</a>
-	     <td style="text-align:left;">
-	     <b>Start: </b>{{printDate(feature.properties.processStart,true)}}<br/>
-	     <b>End: </b>{{printDate(feature.properties.processEnd, true)}}
-	     </td>
-	     
-	     <td style="text-align:center;">
-	     {{printDate(feature.properties.temporalExtent[0])}}
-	     <b>&rarr;</b>
-	     {{printDate(feature.properties.temporalExtent[1])}}
-	     </td>
-	     <td>
-	     <div v-for="type in ['provider', 'position', 'parameters']" class="infos" >
-	         <div v-if="feature.properties[type]"  >
-	            <div v-for="(value, prop) in feature.properties[type]" >
-	              <div><b>{{prop}}:</b> {{value}}</div>
-	            </div>
-	         </div>
-	     </div>
-	     </td>
-	     </tr>
-	     </tbody>
-	   </table>
+		     </td>
+		     </tr>
+		     </tbody>
+		   </table>
 	   </div>
    </div>
   </span>
@@ -164,6 +166,16 @@ export default {
      if (this.parameters.service) {
        url += '&serviceId=' + this.parameters.service.id
      }
+     if (this.parameters.status) {
+       url += '&status=' + this.parameters.status
+     }
+     var self = this
+     var dateType = ['processStart', 'processEnd', 'tempStart', 'tempEnd']
+     dateType.forEach(function (name) {
+       if (self.parameters[name]) {
+         url += '&' + name + '=' + self.parameters[name]
+       }
+     })
      this.$http.get(url, {credentials: true})
       .then(
           response => this.display(response),
@@ -171,6 +183,13 @@ export default {
     },
     pageChange(values) {
       this.pagination = values
+      this.search()
+    },
+    dateChange (e) {
+      var change = {'from': 'Start', 'to': 'End'}
+      var name = e.name.replace('from','Start').replace('to', 'End')
+      console.log(name)
+      this.parameters[name] = e.value
       this.search()
     },
     display (response) {
@@ -202,6 +221,10 @@ export default {
       } else {
         this.parameters.service = service
       }
+      this.search()
+    },
+    statusChange(e) {
+      this.parameters.status = e
       this.search()
     },
     removeSelected(type) {
--- src/subcomponents/gdm-form-process.vue
+++ src/subcomponents/gdm-form-process.vue
@@ -1,47 +1,63 @@
 <i18n>{
    "en":{
-     "results": "Results: {from} to {to} among {notExactly}{count}"
+     "process_extent": "Process Dates",
+     "temporal_extent": "Temporal Extent",
+     "filters": "Filters",
+     "no_filter": "No filter"
    },
    "fr":{
-     "results":  "Résultats: <strong>{from}</strong> à <strong>{to}</strong> sur {notExactly}{count}"
-   }
+     "process_extent": "Dates du calcul",
+     "temporal_extent": "Etendue temporelle",
+     "filters": "Filtres",
+     "no_filter": "Aucun filtre"
+    }
 }
 </i18n>
 <template>
- <span class="gdm-form-process">
+ <div class="gdm-form-process">
+    <div class="research-map">
+      <gdm-map :features="features"></gdm-map>
+    </div>
+    <div class="research">
   
-
-
+      <h3>Status</h3>
+      <formater-select  :options="statusList"  @input="statusChange"></formater-select>
+    </div>
     <div class="research">
       <h3>{{$t('process_extent')}}</h3>
        <div style="max-width:200px;">
-       <formater-temporal-search name="process" lang="fr" daymin="2020-03-01" @change="processDateChange"></formater-temporal-search>
+       <formater-temporal-search name="process" lang="fr" daymin="2020-03-01" @change="dateChange"></formater-temporal-search>
        </div>
     </div>
     <div class="research">
 	    <h3>{{$t('temporal_extent')}}</h3>
 	    <div style="max-width:200px;">
-	       <formater-temporal-search name="temp" lang="fr" daymin="2014-04-03" @change="tempDateChange"></formater-temporal-search>
+	       <formater-temporal-search name="temp" lang="fr" daymin="2014-04-03" @change="dateChange"></formater-temporal-search>
 	    </div>
     </div>
+  
     <div class="research">
      <h3>{{$t('filters')}}</h3>
+   <div v-if="!user && !service" style="font-style:italic;color:grey;">{{$t('no_filter')}}</div>
    <div v-if="user" class="selected">{{user.email}}<span class="fa fa-close" @click="removeFilter('user')"></span></div>
    <div v-if="service" class="selected">{{service.name}}<span class="fa fa-close" @click="removeFilter('service')"></span></div>
   </div>
   <div style="clear:both;"></div>
+</div>
 
- </span>
 </template>
 <script>
 // import AerisDatepicker from 'aeris-commons-components-vjs/src/aeris-datepicker/aeris-datepicker.vue';
 import moment from 'moment';
 // import FormaterTemporalSearch from './formater-temporal-search.vue'
-import {FormaterTemporalSearch} from 'formater-commons-components-vjs'
+import {FormaterTemporalSearch, FormaterSelect} from 'formater-commons-components-vjs'
+import GdmMap from './gdm-map.vue'
 export default {
   name: 'GdmFormProcess',
   components: {
-    FormaterTemporalSearch
+    FormaterTemporalSearch,
+    FormaterSelect,
+    GdmMap
   },
   props: {
     user: {
@@ -63,6 +79,14 @@ export default {
     lang: {
       type: String,
       default: 'en'
+    },
+    features: {
+      type: Array,
+      default: () => ([])
+    },
+    bbox: {
+      type: String,
+      default: null
     }
   },
   computed: {
@@ -84,11 +108,15 @@ export default {
         daymin: '2020-01-01',
         daymax: 'now'
       },
+      statusList: ['WAITING', 'TERMINATED', 'RUNNING', 'FAILED', 'PURGED'],
       dateFormat: 'DD/MM/YYYY',
       pattern: '(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[012])\/[0-9]{4}'
     }
   },
   methods: {
+    getStatus () {
+      
+    },
     removeFilter (type) {
       this.$emit('remove', type)
     },
@@ -107,22 +135,27 @@ export default {
 	          return moment( str, format);
 	      }
 	  },
-	  processDateChange(e) {
-	    console.log('process')
-	    console.log(e)
+	  dateChange(e) {
+	    this.$emit('dateChange', e)
 	  },
-	  tempDateChange(e) {
-	    console.log('temporal')
-	    console.log(e)
+	  statusChange(e) {
+	    if (e === '---') {
+	      e = null
+	    }
+	    this.$emit('statusChange', e)
 	  }
   }
 }
 </script>
 <style scoped>
-.gdm-form-process > div:first-child {
+.gdm-form-process {
+  border: 1px solid grey;
+  border-radius: 3px;
   margin-bottom: 30px;
   min-height: 20px;
+  padding: 10px;
 }
+
 .gdm-form-process .fa-close{
   color: darkred;
   margin-left:10px;
@@ -131,15 +164,16 @@ export default {
 }
 .gdm-form-process div.selected{
   float:left;
-  padding: 5px;
-  margin: 5px;
-  background: lightgrey;
+  padding: 3px;
+  margin: 0px 5px 5px 0px;
+  background: #F0f0f0;
   border: 1px solid grey;
   border-radius:2px;
 }
 .gdm-form-process div.research{
   float:left;
   margin-right: 10px;
+  max-width:300px;
 }
 
 </style>
