3250c0e4e21fd44f10a43b3aaf4824151caf10a3
epointal <elisabeth.pointal@free.fr>
2020-06-04 09:48:09 +0200

    some buttons to process view

--- src/gdm-process-view.vue
+++ src/gdm-process-view.vue
@@ -9,16 +9,54 @@
 </i18n>
 <template>
  <span class="gdm-process-view">
- <h1>@todo</h1>
- <div>{{process.up_name }}</div>
- <div>{{process.s_name}} - {{process.up_id}}</div>
- <div>Status: {{process.status}}</div>
- <diV>Dates process: {{date2str(process.up_date)}} <span class="fa fa-long-arrow-right"></span> {{date2str(process.ps_date)}}</diV>
- <div></div>
- <div style="width:300px;">
-  <gdm-map :feature-collection="feature"></gdm-map>
+ <h1>@todo - {{process.s_name}} N°{{process.up_id}}<span v-if="process.up_name"> - {{process.up_name }}</span></h1>
+ <div class="gdm-process-header">
+   <div class="header-1">
+     <div style="width:300px;margin:auto;">
+      <gdm-map :feature-collection="feature"></gdm-map>
+     </div>
+      <div style="text-align:center;">
+         {{date2str(process.up_start, true)}}
+         <span class="fa fa-long-arrow-right"></span>
+         {{date2str(process.up_end, true)}}
+     </div>
+   </div>
+   <div class="header-2">
+      <div>Status: {{process.status}}</div>
+      <div>Progress: {{process.progress}}</div>
+      <div>
+        Dates process: {{date2str(process.up_date)}}
+        <span class="fa fa-long-arrow-right"></span>
+        {{date2str(process.ps_date)}}
+      </div>
+   </div>
+   <div class="header-3">
+    <div v-if="back">
+    </div>
+    <div v-else>
+       
+      <div v-if="process.status === 'SAVED'">
+       <a class="button" :href="url + 'process/' + process.up_id + '/edit'">Edit</a>
+       <a class="button" @click="todo" disabled>Evaluate</a>
+      </div>
+      <div v-if="process.status === 'EVALUATED'">
+       <a class="button" :href="url + 'process/' + process.up_id + '/edit'">Edit</a>
+       <a class="button" @click="todo">Launch</a>
+      </div>
+      <div v-if="process.status === 'WAITING'">
+       <a class="button" :href="url + 'process/' + process.up_id + '/edit'">Edit</a>
+      
+       <a class="button" @click="todo">Launch</a>
+      </div>
+      <div v-if="process.status === 'RUNNING'">
+        <a class="button" @click="todo" disabled>Get Status (en attendant)</a>
+        <a class="button" @click="todo" disabled>STOP</a>
+      </div>
+     
+     </div>
+   </div>
  </div>
-  <div>TempExtent: {{date2str(process.up_start, true)}} <span class="fa fa-long-arrow-right"></span> {{date2str(process.up_end, true)}}</div>
+ <h2>Parameters</h2>
   <div v-for="(value, prop) in parameters" style="font-size:0.9rem;">
     <b>{{prop}}:</b> {{value}}
   </div>
@@ -48,6 +86,14 @@ export default {
     lang: {
       type: String,
       default: 'en'
+    },
+    url: {
+      type: String,
+      default: 'http:localhost:8080/#/'
+    },
+    back: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {
@@ -90,11 +136,32 @@ export default {
       this.feature = response.up_parameters
       this.feature.properties.id = this.id
       this.process = response
+    },
+    todo () {
+      alert('TODO')
     }
   }
 }
 </script>
 <style scoped>
 .gdm-process-view{
+}
+.gdm-process-header {
+  display:grid;
+  grid-template-columns: 310px 1fr 1fr;
+  grid-gap: 1Opx;
+  grid-auto-rows: auto;
+}
+.header-1 {
+  grid-column: 1;
+  grid-row: 1/3;
+}
+.header-2 {
+  grid-column: 2;
+  grid-row: 1/3;
+}
+.header-3 {
+  grid-column: 3;
+  grid-row: 1/3;
 }
 </style>
