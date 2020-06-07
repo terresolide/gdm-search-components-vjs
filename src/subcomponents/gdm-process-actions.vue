7d544fa77dcd6136a24962d2c25913a39d00daa2
epointal <elisabeth.pointal@free.fr>
2020-06-07 22:28:29 +0200

    add class disabled

--- src/subcomponents/gdm-process-actions.vue
+++ src/subcomponents/gdm-process-actions.vue
@@ -28,13 +28,14 @@
     </div>
     <div v-else>
        <div v-if="process.status === 'ACCEPTED'">
-        <a  class="button" @click="getStatus" :disabled="!serviceOpen || submitting">{{$t('refresh')}}</a>
+        <a  class="button" @click="getStatus" :class="{disabled: disabled}" :disabled="disabled">{{$t('refresh')}}</a>
       </div>
       <div v-else-if="process.status === 'EVALUATED'">
-      <a  class="button" @click="getStatus" :disabled="!serviceOpen || submitting">{{$t('refresh')}}</a>
+      <a  class="button" @click="getStatus" :class="{disabled: !serviceOpen || submitting}" :disabled="disabled">{{$t('refresh')}}</a>
     
          <a class="button" :href="url + 'process/' + process.id + '/edit'">{{$t('edit')}}</a>
-         <a class="button" @click="launch" :disabled="!serviceOpen || !hasCredit || submitting">{{$t('launch')}}</a>
+         <a class="button" @click="launch" :class="{disabled: disabled || !hasCredit}"
+         :disabled="disabled || !hasCredit">{{$t('launch')}}</a>
       </div>
        <div v-else-if="process.status === 'FAILED'">
          <a class="button" @click="purge" disabled>{{$t('purge')}}</a>
@@ -43,24 +44,26 @@
         <a class="button" :href="url + 'process/' + process.id + '/edit'">{{$t('edit')}}</a>
       </div>
       <div v-else-if="process.status === 'CANCELED'">
-         <a class="button" v-if="isOptic" @click="todo" :disabled="!serviceOpen || submitting">{{$t('relaunch')}}</a>
+         <a class="button" v-if="isOptic" @click="todo" :class="{disabled: disabled}"
+          :disabled="disabled">{{$t('relaunch')}}</a>
       </div>
       <!--  PURGED NOTHING TO DO -->
       <div v-else-if="process.status === 'RUNNING'">
-        <a class="button" @click="getStatus" :disabled="!serviceOpen || submitting">{{$t('refresh')}}</a>
-        <a class="button" @click="dismiss" :disabled="!serviceOpen || submitting">
+        <a class="button" @click="getStatus" :class="{disabled: disabled}" :disabled="disabled">{{$t('refresh')}}</a>
+        <a class="button" @click="dismiss" :class="{disabled: disabled}" :disabled="disabled">
            <span v-if="process.format.indexOf('sar') >= 0">{{$t('stop')}}</span>
            <span v-else >{{$t('cancel')}}</span>
         </a>
       </div>
       <div v-else-if="process.status === 'SAVED'">
          <a class="button"  :href="url + 'process/' + process.id + '/edit'">{{$t('edit')}}</a>
-         <a class="button" @click="evaluate" :disabled="!serviceOpen || submitting">{{$t('evaluate')}}</a>
+         <a class="button" @click="evaluate" :class="{disabled: disabled}" 
+         :disabled="disabled">{{$t('evaluate')}}</a>
       </div>
       <div v-else-if="process.status === 'WAITING'">
 	       <a class="button" :href="url + 'process/' + process.id + '/edit'">{{$t('edit')}}</a>
-	       <a class="button" v-if="process.format.indexOf('sar') >= 0 " 
-	       :disabled="!serviceOpen || !hasCredit || submitting" @click="launch">
+	       <a class="button" v-if="process.format.indexOf('sar') >= 0 " :class="{disabled: disabled || !hasCredit}"
+	       :disabled="disabled || !hasCredit" @click="launch">
 	         {{$t('launch')}}
 	       </a>
       </div>
@@ -139,6 +142,9 @@ export default {
       } else {
         return false
       }
+    },
+    disabled () {
+      return  (!this.serviceOpen || this.submitting)
     }
   },
   destroyed: function() {
@@ -186,4 +192,40 @@ export default {
 }
 </script>
 <style scoped>
+
+a.button{
+   display: inline-block;
+   margin: 0px 7px 3px 0;
+   padding: 3px 12px;
+   height: auto;
+   line-height: 1.43;
+   white-space: normal;
+   text-align: center;
+   background: #ececea;
+   border-width: 1px;
+   border-style: solid;
+   border-radius: 3px;
+   border-color: #ffffff #d4d4cf #d4d4cf;
+   color: #000;
+   text-decoration: none;
+  /* text-shadow: 0 -1px 1px #bcbcb4, 1px 0 1px #d4d4cf, 0 1px 1px #d4d4cf, -1px 0 1px #bcbcb4;*/
+   vertical-align: top;
+   cursor: pointer;
+   pointer-events: auto;
+   box-sizing: border-box;
+   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.65);
+   opacity: 0.8
+  }
+ a.button:hover{
+   background: #f0f0e6;
+   text-decoration: none;
+   opacity: 1
+ }
+
+ a.button:disabled,
+ a.button.disabled {
+    color: #999;
+    pointer-events: none;
+  }
+
 </style>
