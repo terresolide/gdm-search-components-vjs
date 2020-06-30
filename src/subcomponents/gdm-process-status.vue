3571d4f92aa72b3b35ebf20b50e7ce6f988fe9b3
epointal <elisabeth.pointal@free.fr>
2020-06-30 16:14:39 +0200

    tests

--- src/subcomponents/gdm-process-status.vue
+++ src/subcomponents/gdm-process-status.vue
@@ -78,7 +78,6 @@ export default {
 	        break
 	      case 'SAVED':
 	      case 'EVALUATED':
-	        return 'ACCEPTED';
 	      case 'WAITING':
 	         return 'SAVED';
 	      case 'CANCELED':
